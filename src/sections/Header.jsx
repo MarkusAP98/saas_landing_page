8d4fdff8adeeab21b397847ab992b25acafb3d53
Markus <markus.a.phantha@gmail.com>
2024-10-04 11:57:36 +0300

    refactor: Update Header component with new navigation links, styles, and mobile menu button

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 const NavLink = ({ title }) => (
@@ -8,6 +8,7 @@ const NavLink = ({ title }) => (
 );
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
       <div className="container flex h-14 items-center max-lg:px-5">
@@ -15,7 +16,7 @@ const Header = () => {
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
         </a>
 
-        <div className="w-full max-lg:fixed max-lg:top-0 maxlg:left-0 max-lg:w-full max-lg:bg-s2">
+        <div className="w-full max-lg:fixed max-lg:top-0 maxlg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -44,6 +45,12 @@ const Header = () => {
             </nav>
           </div>
         </div>
+        <button
+          className="lh:hidden z-2 size-10 border-2 border.s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((prevState) => !prevState)}
+        >
+          <img src={`/images/${isOpen ? "close" : "magic"}.svg`} alt="" />
+        </button>
       </div>
     </header>
   );
